23c5aad254117d529b7de07b359830e67577ac4a
David McClure <david.mcclure@virginia.edu>
2013-10-04 19:14:34 -0700

    Sketches line rendering, ready for refactor.

--- views/shared/javascripts/payloads/lines-public.js
+++ views/shared/javascripts/payloads/lines-public.js
@@ -9025,13 +9025,40 @@ Neatline.module('Lines', function(
    */
   var highlight = function(args) {
 
+    /*
+
     // Did the event originate on the map or text?
     if (args.source == 'MAP' || args.source == 'TEXT') {
 
       // Try to get a vector layer for the model.
-      console.log(Neatline.Map.__view.layers.vector[args.model.id]);
+      var layer = Neatline.Map.__view.layers.vector[args.model.id];
+      var centroid = layer.getDataExtent().getCenterPixel();
+
+      // Try to get a text span for the model.
+      var slug = args.model.get('slug');
+      var span = Neatline.Text.__view.getSpansWithSlug(slug);
+      var offset = span.offset();
+      var x = offset.left+span.width()/2;
+      var y = offset.top+span.height()/2;
+
+      // Measure the window.
+      var width = $(window).width();
+      var height = $(window).height();
+
+      // Add the SVG container.
+      var svg = d3.select('body').append('svg:svg')
+        .attr('width', width)
+        .attr('height', height);
+
+      var line = svg.append('svg:line')
+        .attr('x1', x)
+        .attr('y1', y)
+        .attr('x2', centroid.x)
+        .attr('y2', centroid.y);
 
     }
+
+    */
 
   };
   Neatline.commands.setHandler(Lines.ID+':highlight', highlight);
