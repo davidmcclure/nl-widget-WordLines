84668374341e9686d91eae0567544aa68adb18a8
David McClure <david.mcclure@virginia.edu>
2013-10-06 11:44:29 -0700

    Break when either (a) layer has no features or (b) text span doesn't exist.

--- views/shared/javascripts/payloads/lines-public.js
+++ views/shared/javascripts/payloads/lines-public.js
@@ -9030,16 +9030,20 @@ Neatline.module('Lines', function(
       // Did the event originate on the map or text?
       if (_.contains(['MAP', 'TEXT'], args.source)) {
 
-        // Get the vector geometry centroid.
+        // Get the vector layer.
         var layer = Neatline.request('MAP:getVectorLayer', args.model);
+        if (layer.features.length == 0) return;
+
+        // Computer the map center.
         var lonlat = layer.getDataExtent().getCenterLonLat();
         var center = layer.getViewPortPxFromLonLat(lonlat);
 
         // Get the text span and offset.
         var span = Neatline.request('TEXT:getSpans', args.model);
-        var offset = span.offset();
+        if (span.length == 0) return;
 
-        // Compute the text centroid.
+        // Compute the text center.
+        var offset = span.offset();
         var x = offset.left+span.width()/2;
         var y = offset.top+span.height()/2;
 
